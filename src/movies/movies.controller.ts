81a989d9f5dd39215bb98b673125b451511f7f70
Guiwoo <park.guiwoo@hotmail.com>
2021-12-07 23:52:48 +0900

    validationpipe, api checker

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,40 +1,42 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query,} from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
 
 @Controller('movies')
 export class MoviesController {
  
+    constructor(private readonly movieService: MoviesService){}
+
     @Get()
-    getAll(){
-        return "This will return all movies"
+    getAll():Movie[] {
+        return this.movieService.getAll();
     }
 
-    @Get("search")
-    search(@Query('year') searchingYear:string){
-        return `We are searching for a movie with a title ${searchingYear}`
-    }
+    // @Get("search")
+    // search(@Query('year') searchingYear:string){
+    //     return `We are searching for a movie with a title ${searchingYear}`
+    // }
 
     @Get("/:id")
-    getOne(@Param("id") movieId: string){
-        return `This will return one movie id :${movieId}`
+    getOne(@Param("id") movieId: number): Movie{
+        return this.movieService.getOne(movieId)
     }
 
     @Post()
-    create(@Body() movieData){
-        console.log(movieData)
-        return "This will create a movie"
+    create(@Body() movieData:CreateMovieDto){
+        return this.movieService.create(movieData)
     }
 
     @Delete("/:id")
-    remove(@Param('id') movieId:string){
-        return `This will delete a movie id: ${movieId}`
+    remove(@Param('id') movieId:number){
+        return this.movieService.deleteOne(movieId)
     }
     
     @Patch(`/:id`)
-    path(@Param('id') movieId: string, @Body() updateData){
-        return {
-            updatedMovie: movieId,
-            ...updateData
-        }
+    path(@Param('id') movieId: number, @Body() updateData:UpdateMovieDto){
+        return this.movieService.update(movieId,updateData)
     }
 
 }
